cbca3589026bf2e346f08e9866d5ba7bb139ad5f
Michel DUPONT <ext.capgemini.michel.dupont@sncf.fr>
2017-09-15 16:43:34 +0200

    initial

---
+++ src/main/java/com/dupont/michel/springBoot/Application.java
@@ -0,0 +1,15 @@
+package com.dupont.michel.springBoot;
+
+/**
+ * Created by PMDT08841 on 14/09/2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
