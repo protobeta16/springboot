31dcb8e4ad4323f36dd13b36dd925a4ffad4a32e
Michel DUPONT <ext.capgemini.michel.dupont@sncf.fr>
2017-10-06 16:58:34 +0200

    Ajout du circuit breaker

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # springboot tutorials
-
+here is my personal test of springboot demo
 ## Demo 1 : Building a RESTful Web Service 
 [tuto](https://spring.io/guides/gs/rest-service/)
 >package : demoRestFullWebService
@@ -8,4 +8,7 @@
 >package : demoSecuringWebApplication
 ## Demo 3 : Building a Hypermedia-Driven RESTful Web Service
 [tuto](https://spring.io/guides/gs/rest-hateoas/) 
+>package : demoRESTHateoas
+## Demo 4 : CircuitBrakers
+[tuto](https://spring.io/guides/gs/circuit-breaker/) 
 >package : demoRESTHateoas
--- bookstore/src/main/java/hello/BookstoreApplication.java
+++ bookstore/src/main/java/hello/BookstoreApplication.java
@@ -1,9 +1,9 @@
 package hello;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.web.bind.annotation.RestController;
-        import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @SpringBootApplication
